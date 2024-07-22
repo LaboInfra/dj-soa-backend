1ebbe300973d1811d0ec30935e948758a810bd1b
msterhuj <gabin.lanore@gmail.com>
2024-07-22 23:09:55 +0200

    fix: login view opti

--- account/views.py
+++ account/views.py
@@ -1,25 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from .forms import UserLogin as UserLoginForm
-from .models import User
 
 
 # Create your views here.
-def login(request):
+def login_view(request):
     if request.method == "POST":
         user_login = UserLoginForm(request.POST)
         if user_login.is_valid():
             email = user_login.cleaned_data.get("email")
             password = user_login.cleaned_data.get("password")
 
-            # check if user exists
-            if not User.objects.filter(email=email).exists():
-                user_login.add_error(None, "Invalid credentials")
-                return render(request, "account/login.html", {"form": user_login})
-
-            user = User.objects.get(email=email)
-            if user.check_password(password):
-                return render(request, "account/login.html", {"form": user_login})
-
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("dashboard")
             user_login.add_error(None, "Invalid credentials")
             return render(request, "account/login.html", {"form": user_login})
 

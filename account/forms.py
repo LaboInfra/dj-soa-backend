2e0371d55af175d6b94e10df5fbfdba23936f0e7
msterhuj <gabin.lanore@gmail.com>
2024-07-22 22:59:04 +0200

    feat: added logic to login page

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm as DjangoUserCreationForm
 from django.contrib.auth.forms import UserChangeForm as DjangoUserChangeForm
+from django import forms
 
 from .models import User
 
@@ -16,3 +17,18 @@ class UserChangeForm(DjangoUserChangeForm):
     class Meta:
         model = User
         fields = ("email",)
+
+
+class UserLogin(forms.Form):
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'your@email.com'
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'your password'
+        })
+    )
--- account/views.py
+++ account/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-
+from .forms import UserLogin as UserLoginForm
+from .models import User
 
 # Create your views here.
 def login(request):
-    return render(request, "account/login.html")
+    if request.method == "POST":
+        user_login = UserLoginForm(request.POST)
+        if user_login.is_valid():
+            email = user_login.cleaned_data.get("email")
+            password = user_login.cleaned_data.get("password")
+
+            # check if user exists
+            if not User.objects.filter(email=email).exists():
+                user_login.add_error(None, "Invalid credentials")
+                return render(request, "account/login.html", {"form": user_login})
+
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return render(request, "account/login.html", {"form": user_login})
+
+            user_login.add_error(None, "Invalid credentials")
+            return render(request, "account/login.html", {"form": user_login})
+
+    else:
+        user_login = UserLoginForm()
+    return render(request, "account/login.html", {"form": user_login})
